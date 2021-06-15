7c77216bbeb074b88130af744c8a562a2ce00a61
om9022 <omkarchikane607@gmail.com>
2021-06-15 19:43:27 +0530

    attendance

--- empWagecomputation.sh
+++ empWagecomputation.sh
@@ -1,3 +1,18 @@
 #! /bin/bash/ -x
 
 echo "Welcome to Empcomputation"
+#! /bin/bash -x
+
+# constant
+IS_PRESENT=1
+
+# variable
+empcheck=$(( RANDOM % 2 ))
+
+# selection
+if [ $empcheck -eq $IS_PRESENT ]
+then 
+      echo "employee is present"
+else
+      echo "employee is absent"
+fi
